e52011ec9fe4ea41b796d6122d61c4b1e160eb3e
Marcos Landi <marcoswinkellandi@gmail.com>
2021-04-12 15:20:28 -0300

    finished generating all images

---
+++ relatorio.md
@@ -0,0 +1,17 @@
+# Trabalho de Implementação 2
+
+Marcos Samuel Winkel Landi - 00304688
+
+## 1 - a)
+
+Comparando os tamanhos de vizinhança, é possível ver que com uma vizinhança menor o detalhe é maior mas o ruído também, ao passo que com uma vizinhança maior, as texturas são mais suaves porém o detalhe é diminuido, como pode-se observar nas pontas dos chapéus de aniversário.
+
+![dispSSDfull](C:\Users\Marcos\Desktop\facul\visao\T2\dispSSDfull.png)
+
+## 1 - b)
+
+Foi usada a distância soma dos mínimos quadrados no espaço de cores L\*a\*b\*, que é um espaço de cores mais perceptualmente uniformes que RGB ou BGR.
+
+![LAB_Comparison_Full](C:\Users\Marcos\Desktop\facul\visao\T2\LAB_Comparison_Full.png)
+
+As diferenças não são facilmente perceptíveis ao compararmos os resultados lado a lado, por isso gifs comparativos podem ser encontrados em github.com/mswlandi/stereoAssignment
--- stereo.py
+++ stereo.py
@@ -54,6 +54,9 @@ def disparityMap(in1, in2, N, disparity_max=63, distanceType="SSD"):
     if(distanceType == "LAB"):
         img1 = cv2.cvtColor(in1, cv2.COLOR_BGR2LAB)
         img2 = cv2.cvtColor(in2, cv2.COLOR_BGR2LAB)
+    else:
+        img1 = in1
+        img2 = in2
 
     for y in range(0, h):
         for x1 in range(0, w):
@@ -61,7 +64,7 @@ def disparityMap(in1, in2, N, disparity_max=63, distanceType="SSD"):
             min_cost_pos = 0 # posição x do pixel de menor custo na segunda imagem
 
             for x2 in range(max(0, x1 - disparity_max), min(w, x1 + disparity_max)):
-                current_cost = regionFunction(in1, in2, N, y, x1, x2, distanceType)
+                current_cost = regionFunction(img1, img2, N, y, x1, x2, distanceType)
 
                 if (current_cost < min_cost):
                     min_cost = current_cost
@@ -74,7 +77,7 @@ def disparityMap(in1, in2, N, disparity_max=63, distanceType="SSD"):
     return disparity
 
 
-def disparityMapAggregation(in1, in2, N, M, disparity_max=63, distanceType="SSD"):
+def disparityMapAggregation(in1, in2, N, M, disparity_max=63, distanceType="SSD", aggregationType="median"):
     h = in1.shape[0]
     w = in1.shape[1]
 
@@ -104,7 +107,10 @@ def disparityMapAggregation(in1, in2, N, M, disparity_max=63, distanceType="SSD"
                 costs[y, x1] = cost
         
         # agregação em si
-        costs = ndimage.median_filter(costs, size=M)
+        if (aggregationType == "median"):
+            costs = ndimage.median_filter(costs, size=M)
+        else:
+            costs = ndimage.uniform_filter(costs, size=M)
 
         # coloca na lista de imagens de custos
         costVolume.append(costs)
@@ -126,120 +132,116 @@ def disparityMapAggregation(in1, in2, N, M, disparity_max=63, distanceType="SSD"
     return disparityImage
 
 
-def geraImagensExercicio1a():
-    in1 = cv2.imread('im2.png')
-    in2 = cv2.imread('im6.png')
-    disp1 = cv2.imread('disp2.png')
-    disp2 = cv2.imread('disp6.png')
-
-    fig = plt.figure(figsize=(15, 20), dpi=80)
-    ax = fig.add_subplot(3, 2, 1)
-    ax.title.set_text('Original')
-    ax.axis('off')
-    plt.imshow(in1[..., ::-1])
-
-    ax = fig.add_subplot(3, 2, 2)
-    ax.title.set_text('Ground Truth')
-    ax.axis('off')
-    plt.imshow(disp1[...,::-1])
-
-    disparity11 = disparityMap(in1, in2, 1)
-    plt.imsave('dispSSD1x1.png', disparity11)
-
-    ax = fig.add_subplot(3, 2, 3)
-    ax.title.set_text('Neighbourhood = 1x1')
-    ax.axis('off')
-    plt.imshow(disparity11[...,::-1])
-
-    disparity33 = disparityMap(in1, in2, 3)
-    plt.imsave('dispSSD3x3.png', disparity33)
-
-    ax = fig.add_subplot(3, 2, 4)
-    ax.title.set_text('Neighbourhood = 3x3')
-    ax.axis('off')
-    plt.imshow(disparity33[..., ::-1])
-
-    disparity55 = disparityMap(in1, in2, 5)
-    plt.imsave('dispSSD5x5.png', disparity55)
-
-    ax = fig.add_subplot(3, 2, 5)
-    ax.title.set_text('Neighbourhood = 5x5')
-    ax.axis('off')
-    plt.imshow(disparity5[..., ::-1])
-
-    disparity1111 = disparityMap(in1, in2, 11)
-    plt.imsave('dispSSD11x11.png', disparity1111)
-
-    ax = fig.add_subplot(3, 2, 6)
-    ax.title.set_text('Neighbourhood = 11x11')
-    ax.axis('off')
-    plt.imshow(disparity1111[..., ::-1])
-
-    fig.tight_layout(h_pad=1.5, w_pad=1.0)
-    fig.savefig('dispSSDfull.png')
+def appendImages(img1, img2):
+    return np.append(np.append(img1, np.full((img1.shape[0], 15, 3), np.uint8(255)), axis=1), img2, axis=1)
 
 
-def geraImagensExercicio1b():
-    in1 = cv2.imread('im2.png')
-    in2 = cv2.imread('im6.png')
-    disp1 = cv2.imread('disp2.png')
-    disp2 = cv2.imread('disp6.png')
 
-    fig = plt.figure(figsize=(15, 20), dpi=80)
-    ax = fig.add_subplot(3, 2, 1)
-    ax.title.set_text('Original')
-    ax.axis('off')
-    plt.imshow(in1[..., ::-1])
+# Gera imagens para a primeira parte (a) do exercício 1
+in1 = cv2.imread('im2.png')
+in2 = cv2.imread('im6.png')
+disp1 = cv2.imread('disp2.png')
+disp2 = cv2.imread('disp6.png')
 
-    ax = fig.add_subplot(3, 2, 2)
-    ax.title.set_text('Ground Truth')
-    ax.axis('off')
-    plt.imshow(disp1[..., ::-1])
+fig = plt.figure(figsize=(15, 20), dpi=80)
+ax = fig.add_subplot(3, 2, 1)
+ax.title.set_text('Original')
+ax.axis('off')
+plt.imshow(in1[..., ::-1])
 
-    disparity11 = disparityMap(in1, in2, 1, distanceType="LAB")
-    plt.imsave('dispSSD1x1_LAB.png', disparity11)
+ax = fig.add_subplot(3, 2, 2)
+ax.title.set_text('Ground Truth')
+ax.axis('off')
+plt.imshow(disp1[...,::-1])
 
-    ax = fig.add_subplot(3, 2, 3)
-    ax.title.set_text('Neighbourhood = 1x1')
-    ax.axis('off')
-    plt.imshow(disparity11[..., ::-1])
+disparity11 = disparityMap(in1, in2, 1)
+plt.imsave('dispSSD1x1.png', disparity11)
 
-    disparity33 = disparityMap(in1, in2, 3, distanceType="LAB")
-    plt.imsave('dispSSD3x3_LAB.png', disparity33)
+ax = fig.add_subplot(3, 2, 3)
+ax.title.set_text('Neighbourhood = 1x1')
+ax.axis('off')
+plt.imshow(disparity11[...,::-1])
 
-    ax = fig.add_subplot(3, 2, 4)
-    ax.title.set_text('Neighbourhood = 3x3')
-    ax.axis('off')
-    plt.imshow(disparity33[..., ::-1])
+disparity33 = disparityMap(in1, in2, 3)
+plt.imsave('dispSSD3x3.png', disparity33)
 
-    disparity55 = disparityMap(in1, in2, 5, distanceType="LAB")
-    plt.imsave('dispSSD5x5_LAB.png', disparity55)
+ax = fig.add_subplot(3, 2, 4)
+ax.title.set_text('Neighbourhood = 3x3')
+ax.axis('off')
+plt.imshow(disparity33[..., ::-1])
 
-    ax = fig.add_subplot(3, 2, 5)
-    ax.title.set_text('Neighbourhood = 5x5')
-    ax.axis('off')
-    plt.imshow(disparity5[..., ::-1])
+disparity55 = disparityMap(in1, in2, 5)
+plt.imsave('dispSSD5x5.png', disparity55)
 
-    disparity1111 = disparityMap(in1, in2, 11, distanceType="LAB")
-    plt.imsave('dispSSD11x11_LAB.png', disparity1111)
+ax = fig.add_subplot(3, 2, 5)
+ax.title.set_text('Neighbourhood = 5x5')
+ax.axis('off')
+plt.imshow(disparity55[..., ::-1])
 
-    ax = fig.add_subplot(3, 2, 6)
-    ax.title.set_text('Neighbourhood = 11x11')
-    ax.axis('off')
-    plt.imshow(disparity1111[..., ::-1])
+disparity1111 = disparityMap(in1, in2, 11)
+plt.imsave('dispSSD11x11.png', disparity1111)
 
-    fig.tight_layout(h_pad=1.5, w_pad=1.0)
-    fig.savefig('dispLABfull.png')
+ax = fig.add_subplot(3, 2, 6)
+ax.title.set_text('Neighbourhood = 11x11')
+ax.axis('off')
+plt.imshow(disparity1111[..., ::-1])
 
-
-def geraImagensExercicio2():
-    in1 = cv2.imread('im2.png')
-    in2 = cv2.imread('im6.png')
-    disp1 = cv2.imread('disp2.png')
-    disp2 = cv2.imread('disp6.png')
-
-    disparityAgg = disparityMapAggregation(in1, in2, 1, 3, distanceType="SSD")
-    plt.imsave('dispSSD1x1_Agg.png', disparityAgg)
+fig.tight_layout(h_pad=1.5, w_pad=1.0)
+fig.savefig('dispSSDfull.png')
 
 
-geraImagensExercicio2()
+# Gera imagens para a segunda parte (b) do exercício 1
+in1 = cv2.imread('im2.png')
+in2 = cv2.imread('im6.png')
+
+disparityLAB11 = disparityMap(in1, in2, 1, distanceType="LAB")
+plt.imsave('dispSSD1x1_LAB.png', disparityLAB11)
+plt.imsave('dispSSD1x1_LAB_Comparison.png', appendImages(disparity11, disparityLAB11))
+
+disparityLAB33 = disparityMap(in1, in2, 3, distanceType="LAB")
+plt.imsave('dispSSD3x3_LAB.png', disparityLAB33)
+plt.imsave('dispSSD3x3_LAB_Comparison.png', appendImages(disparity33, disparityLAB33))
+
+disparityLAB55 = disparityMap(in1, in2, 5, distanceType="LAB")
+plt.imsave('dispSSD5x5_LAB.png', disparityLAB55)
+plt.imsave('dispSSD5x5_LAB_Comparison.png', appendImages(disparity55, disparityLAB55))
+
+disparityLAB1111 = disparityMap(in1, in2, 11, distanceType="LAB")
+plt.imsave('dispSSD11x11_LAB.png', disparityLAB1111)
+plt.imsave('dispSSD11x11_LAB_Comparison.png', appendImages(disparity1111, disparityLAB1111))
+
+# Gera imagens para o exercício 2
+# Imagens com filtro de média
+disparityMeanAgg11 = disparityMapAggregation(in1, in2, 1, 3, distanceType="SSD", aggregationType="mean")
+plt.imsave('dispMeanAgg1x1_Agg.png', disparityMeanAgg11)
+plt.imsave('dispMeanAgg1x1_Agg_Comparison.png', appendImages(disparity11, disparityMeanAgg11))
+
+disparityMeanAgg33 = disparityMapAggregation(in1, in2, 3, 3, distanceType="SSD", aggregationType="mean")
+plt.imsave('dispMeanAgg3x3_Agg.png', disparityMeanAgg33)
+plt.imsave('dispMeanAgg3x3_Agg_Comparison.png', appendImages(disparity33, disparityMeanAgg33))
+
+disparityMeanAgg55 = disparityMapAggregation(in1, in2, 5, 3, distanceType="SSD", aggregationType="mean")
+plt.imsave('dispMeanAgg5x5_Agg.png', disparityMeanAgg55)
+plt.imsave('dispMeanAgg5x5_Agg_Comparison.png', appendImages(disparity55, disparityMeanAgg55))
+
+disparityMeanAgg1111 = disparityMapAggregation(
+    in1, in2, 11, 3, distanceType="SSD", aggregationType="mean")
+plt.imsave('dispMeanAgg11x11_Agg.png', disparityMeanAgg1111)
+plt.imsave('dispMeanAgg11x11_Agg_Comparison.png', appendImages(disparity1111, disparityMeanAgg1111))
+
+# Imagens com filtro de mediana
+disparityMedianAgg11 = disparityMapAggregation(in1, in2, 1, 3, distanceType="SSD", aggregationType="median")
+plt.imsave('dispMedianAgg1x1_Agg.png', disparityMedianAgg11)
+plt.imsave('dispMedianAgg1x1_Agg_Comparison.png', appendImages(disparity11, disparityMedianAgg11))
+
+disparityMedianAgg33 = disparityMapAggregation(in1, in2, 3, 3, distanceType="SSD", aggregationType="median")
+plt.imsave('dispMedianAgg3x3_Agg.png', disparityMedianAgg33)
+plt.imsave('dispMedianAgg3x3_Agg_Comparison.png', appendImages(disparity33, disparityMedianAgg33))
+
+disparityMedianAgg55 = disparityMapAggregation(in1, in2, 5, 3, distanceType="SSD", aggregationType="median")
+plt.imsave('dispMedianAgg5x5_Agg.png', disparityMedianAgg55)
+plt.imsave('dispMedianAgg5x5_Agg_Comparison.png', appendImages(disparity55, disparityMedianAgg55))
+
+disparityMedianAgg1111 = disparityMapAggregation(in1, in2, 11, 3, distanceType="SSD", aggregationType="median")
+plt.imsave('dispMedianAgg11x11_Agg.png', disparityMedianAgg1111)
+plt.imsave('dispMedianAgg11x11_Agg_Comparison.png', appendImages(disparity1111, disparityMedianAgg1111))
